c07e27ef0172b4310e17a36248fbcd09a9b74e4c
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-10-28 00:51:31 -0300

    using 3ª party modules: slugify

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
+
 const replaceTemplete = require('./modules/replaceTemplate'); 
 
 
@@ -39,6 +42,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/templete-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
 
 //Call back function is executed each time tha it is requested
 const server = http.createServer((req, res) => {
