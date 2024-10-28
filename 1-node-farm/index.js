8074343a75d2de680405305a374c5c64f92deb96
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-10-28 00:03:46 -0300

    Modules: creating and exporting my own modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplete = require('./modules/replaceTemplate'); 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -31,21 +32,6 @@ const url = require('url');
 ////////////////////////////////////////////////////////////////////////////////////////////
 // SERVER
 //Top level code is only executed once, when the application starts
-const replaceTemplete = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic){
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/templete-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/templete-card.html`, 'utf-8')
