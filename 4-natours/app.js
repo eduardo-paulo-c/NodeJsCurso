0da935a76b76f153748bfc7307e05fc7f3d5fc34
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-11 18:00:23 -0300

    Starting our API, GET, POST and URLs parameters

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,14 +1,67 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ messege: 'Hello from the server side!', app: 'narours' });
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ messege: 'Hello from the server side!', app: 'narours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'sucess',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+////////'/api/v1/tours/:id/:x/:y?'
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      tour,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 });
 
 app.listen(port, () => {
