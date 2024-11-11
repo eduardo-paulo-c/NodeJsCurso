e26f98844d2961f4c6bc5c24c35bfa1bf9f499ed
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-11 13:44:21 -0300

    Multiple promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -24,17 +24,43 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-    console.log(res.body.message);
+    const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 
-    await writeFilePro('dog-img.txt', res.body.message);
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Randon dog image saved to file!');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: READY';
 };
 
-getDogPic();
+(async () => {
+  try {
+    console.log('1: Wil get dog pics');
+    x = await getDogPic();
+    console.log(x);
+    console.log('3: Done geting dog pics');
+  } catch (err) {
+    console.log(`ERROR!!!`);
+  }
+})();
+
+// console.log('1: Wil get dog pics');
+// getDogPic()
+//   .then((x) => {
+//     console.log(x);
+//     console.log('3: Done geting dog pics');
+//   })
+//   .catch((err) => {
+//     console.log(`ERROR!!!`);
+//   });
 
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then((data) => {
