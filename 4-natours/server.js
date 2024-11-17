e4226f715485c24544c8336edb4e26de003d0992
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-17 06:31:40 -0300

    Setting the environment variables

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,11 @@
-const app = require('./app');
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
+const app = require('./app');
+
+const port = process.env.port || 3000;
+console.log(process.env.NODE_ENV);
 // START SERVER
 app.listen(port, () => {
-  console.log(`Running at port: ${port}...`, port);
+  console.log(`Running at port: ${port}...`);
 });
