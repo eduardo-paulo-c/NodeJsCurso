5728ec098207cfbcd640872812e2b87e423813b0
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-18 17:46:55 -0300

    Setting yo ESLint + Prettier

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
 const port = process.env.port || 3000;
+
 console.log(process.env.NODE_ENV);
 // START SERVER
 app.listen(port, () => {
