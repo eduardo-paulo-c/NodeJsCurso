b38d67a3a547838ed342055ac025827ef59139f6
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-19 11:02:06 -0300

    Connecting to the remote database with mongoose an criating the first model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,11 +1,43 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const { MongoNetworkError } = require('mongodb');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.port || 3000;
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-console.log(process.env.NODE_ENV);
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Db connection successful!');
+  });
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name.'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a name.'],
+  },
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
+
+const port = process.env.port || 3000;
+// console.log(process.env.NODE_ENV);
 // START SERVER
 app.listen(port, () => {
   console.log(`Running at port: ${port}...`);
