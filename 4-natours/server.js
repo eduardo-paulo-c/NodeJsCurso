0be6b802d13e9808e45177165492105b5d328e07
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-19 14:38:48 -0300

    Setting up the MVC architecture

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -18,24 +18,6 @@ mongoose
     console.log('Db connection successful!');
   });
 
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name.'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour must have a name.'],
-  },
-});
-
-const Tour = mongoose.model('Tour', toursSchema);
-
 const port = process.env.port || 3000;
 // console.log(process.env.NODE_ENV);
 // START SERVER
