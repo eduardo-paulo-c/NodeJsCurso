f91bcd400beee6ab635113f93b202ac57b984dc4
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-21 22:08:51 -0300

    Modeling the tours and importing development data

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -3,17 +3,54 @@ const mongoose = require('mongoose');
 const toursSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Tour must have a name.'],
+    required: [true, 'A Tour must have a name.'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A Tour must have a duration.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A Tour must have a group size.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A Tour must have a difficulty'],
+  },
+  ratingsAvarage: {
     type: Number,
     default: 4.5,
   },
+  retingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'Tour must have a price.'],
+    required: [true, 'A Tour must have a price.'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A Tour must have a summary.'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A Tour must have a cover image.'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
