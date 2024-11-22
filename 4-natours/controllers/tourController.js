4080d0c9274c05a5c4a6fad359130913c9b4c9b1
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-11-21 22:37:02 -0300

    Basic filtering

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -13,7 +14,25 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Building a query.
+    const queryObj = { ...req.query };
+    const excludedFilds = ['page', 'sort', 'limit', 'filds'];
+    excludedFilds.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // Execute the query.
+    const tours = await query;
+
+    // Send response
     res.status(200).json({
       status: 'sucess',
       results: tours.length,
