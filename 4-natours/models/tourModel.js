9f46da816bef69c8314d6e9aaf66295fa2e78360
Paulo Eduardo Campos <dudutcampossm1204@gmail.com>
2024-12-06 12:23:19 -0300

    Data validation: Built-in validators

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -8,6 +8,8 @@ const toursSchema = new mongoose.Schema(
       required: [true, 'A Tour must have a name.'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have less or equal than 10 characters'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const toursSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A Tour must have a difficulty'],
+      enum: {
+        values: ['eazy', 'medium', 'difficult'],
+        message: 'Difficult is either: eazy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -94,14 +102,14 @@ toursSchema.pre(/^find/, function (next) {
 });
 
 toursSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   // console.log(docs);
   next();
 });
 
 // Aggregation Middleware
 toursSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  // this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   console.log(this.pipeline());
   next();
 });
